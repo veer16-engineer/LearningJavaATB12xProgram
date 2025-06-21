dc8c3b7d98159e5b546047e20bb95f5ec5067a35
Virbhadra Swami <gkw.veer16@gmail.com>
2025-06-21 12:56:43 +0530

    task done

---
+++ src/Java_Programming_Assessment/Challenge8_LeapYearChecker.java
@@ -0,0 +1,22 @@
+package Java_Programming_Assessment;
+import java.util.Scanner;
+
+public class Challenge8_LeapYearChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+
+        scanner.close();
+    }
+}
